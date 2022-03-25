1c160b774d10698c2dc5e2ab9ef5d8298c45e41d
Kaneka Miller <kmmiller418@gmail.com>
2022-03-25 09:43:50 -0400

    Fix if conditional

--- this.js
+++ this.js
@@ -2,7 +2,7 @@ const photoSlideShow = {
     photoList: ['cool-tree.jpg', 'cool-rock.jpg', 'cool-fruit.jpg'],
     currentPhotoIndex: 0,
     nextPhoto: function(){
-        if (this.currentPhotoIndex < this.photoList.length){
+        if (this.currentPhotoIndex < this.photoList.length - 1){
             this.currentPhotoIndex++;
             console.log(this.photoList[this.currentPhotoIndex]);
         } else {
